f3b99c497b123ec6a8a920573c4aaf0e9c4a0430
oshilim-chukwudi <chukwudioshilim@gmail.com>
2023-07-08 00:35:31 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const computerchoice = document.getElementById("Computer-choice");
+const userchoice = document.getElementById("Your-choice");
+const ResultDisplay = document.getElementById("Result");
+
+const possiblechoices = document.querySelectorAll("button");
